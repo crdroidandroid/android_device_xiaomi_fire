3d01f24c190973aabd859eebf410b533026e2211
1VicTim1 <122aaa121@gmail.com>
2026-09-02 15:40:50 +0500

    fire: leave HOS2 module payload to DLKM partitions

--- kernel/KernelProduct.mk
+++ kernel/KernelProduct.mk
@@ -4,9 +4,5 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-FIRE_KERNEL_VERSION ?= 4.19
-FIRE_KERNEL_6_6_VENDOR_MODULE_LIST ?= device/xiaomi/fire/kernel/6.6/vendor-modules.list
-
-ifeq ($(FIRE_KERNEL_VERSION),6.6)
-PRODUCT_PACKAGES += fire_kernel_6_6_vendor_modules
-endif
+# HOS2 kernel modules are not flattened into /vendor. They are supplied by
+# the stock vendor_boot/vendor_dlkm/system_dlkm/odm_dlkm payload contract.
